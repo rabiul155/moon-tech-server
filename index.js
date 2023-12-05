c2a0b9ba6975b33bb3c8ed943302a86a3c06a4b9
rabiul-repliq <rabiul.islam@repliq.dev>
2023-12-05 12:28:15 +0600

    first commit

--- index.js
+++ index.js
@@ -1,45 +1,74 @@
-require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
-
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://Moon-tech:ZPqNffRN6YkLy4Pk@cluster0.i4cqwjk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 const run = async () => {
   try {
-    const db = client.db("moontech");
-    const productCollection = db.collection("product");
-
+    const productCollection = client.db("Moon-tech").collection("products");
     app.get("/products", async (req, res) => {
-      const cursor = productCollection.find({});
-      const product = await cursor.toArray();
-
+      const query = {};
+      const product = await productCollection.find(query).toArray();
       res.send({ status: true, data: product });
     });
 
     app.post("/product", async (req, res) => {
       const product = req.body;
-
       const result = await productCollection.insertOne(product);
-
       res.send(result);
     });
 
     app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
-
       const result = await productCollection.deleteOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
+
+    app.patch("/product/edit/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          model: data.model,
+          image: data.image,
+          brand: data.brand,
+          status: data.status,
+          price: data.price,
+          keyFeature: [
+            data.keyFeature[0],
+            data.keyFeature[1],
+            data.keyFeature[2],
+            data.keyFeature[3],
+          ],
+        },
+      };
+      const result = await productCollection.updateMany(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await productCollection.findOne({ _id: ObjectId(id) });
       res.send(result);
     });
   } finally {
@@ -53,5 +82,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
